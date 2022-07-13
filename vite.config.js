034b1ac4044f8ff5c9377ed733c33e5e4d181533
Lucemans <luc@lucemans.nl>
2022-07-13 13:41:04 +0200

    Introduce basic global style

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
     build: {
         rollupOptions: {
-            input: ['./src/index.tsx', './src/index2.tsx'],
-            output: {
-                entryFileNames: '[name].js',
-                chunkFileNames: `[name].js`
-            }
+            input: ['./src/index.tsx', './src/index2.tsx', './src/style.scss'],
+                output: {
+                    entryFileNames: '[name].js',
+                    chunkFileNames: '[name].js',
+                    assetFileNames: (data) => data.name == 'style.css' ? '[name].css' : 'assets/[name].[ext]'
+                }
         }
     },
 })
